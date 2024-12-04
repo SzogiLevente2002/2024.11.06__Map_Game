ec27c426cdb056d9f097c4c145fc27df27886fab
SzogiLevente2002 <szogileventeattila@gmail.com>
2024-12-04 10:54:38 +0100

    Durr

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '2024.11.06_Map_Game',
+  base: '2024.11.06__Map_Game',
     build: {
         outDir: 'docs',
         emptyOutDir: true
